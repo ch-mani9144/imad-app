02ddb7f8333f552fa320e384910abd39f9094c72
ch-mani9144 <31075854+ch-mani9144@users.noreply.github.com>
2017-08-20 12:25:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/ui/index.html',function (req,res) {
-    res.sendFile(path.join(_dirname,'ui','index.html'));
+    res.sendFile(path.join(__dirname,'ui','index.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
